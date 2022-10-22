0f5b37b68010599f8b15d884272de6e1f259bb40
Noor <ma8073641@gmail.com>
2022-10-21 18:11:39 -0700

    #

---
+++ backened/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const mongoURI = 'mongodb://localhost:27017'
+
+// mongoose return promises
+const connectToMongo = async () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connected to mongoose successfully");
+    })
+}
+
+module.export = connectToMongo;
