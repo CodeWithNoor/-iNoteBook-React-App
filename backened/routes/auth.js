515f1e07d7f525e19059fe9fbe9c3b5e92775558
Noor <ma8073641@gmail.com>
2022-10-23 03:31:28 -0700

    JwtWebToken Added

--- backened/routes/auth.js
+++ backened/routes/auth.js
@@ -7,7 +7,7 @@ const JWT_SECRET = "Null"; // Signature
 const { body, validationResult } = require('express-validator');
 
 // hit the post request and send the data
-// Create a User using: POST:/api/auth/createuser . Doesn't require Auth . No login required
+// Create a User using: POST:/api/auth/createuser . No login required
 
 router.post('/createuser', [
 
@@ -17,7 +17,7 @@ router.post('/createuser', [
 
     // password must be at least 5 chars long
     body('password', 'Password must be 5 numbers').isLength({ min: 5 }),
-    ]  , async (req, res) => {
+], async (req, res) => {
 
     // --> Endpoint <--
     // console.log(req.body);  // password can not be used in plain text
@@ -33,35 +33,35 @@ router.post('/createuser', [
         return res.status(400).json({ errors: errors.array() });
     }
     // res.send(req.body);
-    
-    // Check whether the user with this email exsist already
+
 
     // try --> if any mistake in create user throw error 
     // The try statement lets you test a block of code for errors.
     // The catch statement lets you handle the error.
     // The throw statement lets you create custom errors.
     // The final statement lets you execute code, after try and catch, regardless of the result.
-    
-
-    try{  
 
 
-        let user = await User.findOne({email: req.body.email})
+    try {
+
+        // Check whether the user with this email exsist already
+
+        let user = await User.findOne({ email: req.body.email })
         // console.log(user)
-    
-        if(user){
-            return res.status(400).json({error: "Sorry a User with this email is alraedy exsist"})
+
+        if (user) {
+            return res.status(400).json({ error: "Sorry a User with this email is alraedy exsist" })
         }
 
         // salt to protect against rainbow table attacks
         // bcrypt.js relies on Web Crypto API's getRandomValues interface to obtain secure random numbers.
 
         // Here plainText pass in hashing form
-        const salt= await bcrypt.genSalt(10)
-        const secPass = await bcrypt.hash(req.body.password,salt)
+        const salt = await bcrypt.genSalt(10)
+        const secPass = await bcrypt.hash(req.body.password, salt)
 
-            // create new user
-            user = await User.create({
+        // create new user
+        user = await User.create({
             name: req.body.name,
             email: req.body.email,
             password: secPass,
@@ -69,24 +69,69 @@ router.post('/createuser', [
 
         // send a Token and id
         const data = {
-            user:{
+            user: {
                 id: 'user.id'
             }
         }
 
         // Synchronously sign
         const authtoken = await jwt.sign(data, JWT_SECRET); // Generate authtoken
-        res.json({authtoken})
+        res.json({ authtoken })
 
         // res.json({"msg": "nice"})
         // res.json(user)
-        
 
-    } 
-        // catch error
-        catch(error) {console.log(error.message)
-        res.status(500).send("some error occured") }
-    
+
+    }
+    // catch error
+    catch (error) {
+        console.log(error.message)
+        res.status(500).send("Internal server error")
+    }
+
+})
+
+
+// Authentication a User using: POST:/api/auth/login . No login required
+// Endpoint login
+
+router.post('/login', [
+    body('email', 'Enter a valide email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+
+    // If there are errors, return bad request and the errors
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email})
+        if(!user){
+            res.status(400).json({error: "Please try to login with correct credentials"})
+        }
+
+    const PasswordCompare = bcrypt.compare(password, hash)
+    if(!PasswordCompare){
+        res.status(400).json({error: "Please try to login with correct credentials"})
+    }
+
+    const data = {
+        user: {
+            id: 'user.id'
+        }
+    }
+    const authtoken = await jwt.sign(data, JWT_SECRET); // Generate authtoken
+    res.json({ authtoken })
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send("Internal server error")
+    }
 })
 
 module.exports = router
