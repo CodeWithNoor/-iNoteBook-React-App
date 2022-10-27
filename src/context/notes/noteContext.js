aeed20b7764a0be3728371d34cf9fd9e0e544034
Noor <ma8073641@gmail.com>
2022-10-27 15:03:37 -0700

    </>

--- src/context/notes/noteContext.js
+++ src/context/notes/noteContext.js
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
-// Here We Create Context and Store it in  NoteContext.
-const noteContext = createContext() // state hold related to notes 
+// Here We Create Context and Store it in NoteContext.
+const noteContext = createContext() // ---> state hold related to notes 
 
 export default noteContext;
