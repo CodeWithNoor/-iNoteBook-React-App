54022ad52e7a8f2e0e2107aa9c30c009b651ad01
Noor <ma8073641@gmail.com>
2022-10-28 13:14:49 -0700

    #

---
+++ redux_app/src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className='container'>
+            <h3><strong>Buy Nike Caps 1000Rs.</strong></h3>
+            <button type="button" class="btn btn-dark btn-sm mx-2">+</button>
+            Add to Cart
+            <button type="button" class="btn btn-dark btn-sm mx-2">-</button>
+        </div>
+    )
+}
+
+export default Shop
