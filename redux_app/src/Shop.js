dc34558c1af25c094b6e8c4eb1021fa97af63356
Noor <ma8073641@gmail.com>
2022-10-28 18:23:50 -0700

    #

--- redux_app/src/Shop.js
+++ redux_app/src/Shop.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Shop = () => {
     return (
-        <div className='container'>
-            <h3><strong>Buy Nike Caps 1000Rs.</strong></h3>
-            <button type="button" class="btn btn-dark btn-sm mx-2">+</button>
-            Add to Cart
-            <button type="button" class="btn btn-dark btn-sm mx-2">-</button>
+        <div className='container m-5'>
+            <h3>Deposit/WithDraw Money</h3>
+            <button type="button" class="btn btn-dark btn-sm mx-2 my-2">-</button>
+            <strong>Update Balance</strong>
+            <button type="button" class="btn btn-dark btn-sm mx-2 my-2">+</button>
         </div>
     )
 }
