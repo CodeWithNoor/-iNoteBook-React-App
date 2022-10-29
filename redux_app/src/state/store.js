ab412649f6db7e2bcda6702df2acc0d55a79e369
Noor <ma8073641@gmail.com>
2022-10-29 12:00:07 -0700

    Redux Store

--- redux_app/src/state/store.js
+++ redux_app/src/state/store.js
@@ -1,10 +1,14 @@
-// asyncchronous function ko reduce kren ma help krta hai
+// asynchronous function ko reduce kren ma help krta hai
 // Middleware is to support asynchronous actions without much boilerplate code
 // This is handy for a variety of tasks, such as expressing asynchronous actions in a concise manner, or logging every action payload.
 
-import { applyMiddleware, createStore } from "redux";
+import { configureStore } from '@reduxjs/toolkit'
+import { applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import reducers from "./reducers";
+import amountReducer from "./reducers/amountReducer";
 
-
- export const store = createStore(reducers, {}, applyMiddleware(thunk))
+const Middleware = applyMiddleware(thunk);
+ export const store = configureStore({
+    reducer: {
+        amount: amountReducer 
+    }, Middleware })
